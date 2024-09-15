24a255e9439e6f9a5a66f128e0568213a2596769
Mykaellen <msylvinalealdias@gmail.com>
2024-09-15 12:27:52 -0300

    first commit

---
+++ src/test/java/br/com/myka/buuking/TestApplication.java
@@ -0,0 +1,11 @@
+package br.com.myka.buuking;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
